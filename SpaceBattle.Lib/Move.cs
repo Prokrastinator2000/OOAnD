91c401023a53492247c8bc9a32295c79ad888521
Miha <mikhail05@list.ru>
2024-11-20 12:26:40 +0600

    feat: add move

---
+++ SpaceBattle.Lib/Move.cs
@@ -0,0 +1,43 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IMoving
+{
+    Vec Position { get; set; }
+    Vec Velocity { get; }
+}
+public interface ICommand
+{
+    public void Execute();
+
+}
+public class MoveCommand : ICommand
+{
+    private readonly IMoving obj;
+    public MoveCommand(IMoving obj)
+    {
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        obj.Position = obj.Position + obj.Velocity;
+    }
+}
+public class Vec
+{
+    public int[] Values { get; set; }
+    public Vec(int[] Values)
+    {
+        this.Values = Values;
+    }
+
+    public static Vec operator +(Vec a, Vec b)
+    {
+        for (var i = 0; i < a.Values.Length; i++)
+        {
+            a.Values[i] += b.Values[i];
+        }
+
+        return a;
+    }
+}
+
---
+++ SpaceBattle.Tests/MoveTest.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+namespace SpaceBattle.Tests;
+public class MoveTest
+{
+    [Fact]
+    public void TestPositive()
+    {
+        var moving = new Mock<IMoving>();
+        moving.SetupGet(m => m.Position).Returns(new Vec(new int[] { 12, 5 }));
+        moving.SetupGet(m => m.Velocity).Returns(new Vec(new int[] { -7, 3 }));
+        var cmd = new MoveCommand(moving.Object);
+        cmd.Execute();
+
+        moving.VerifySet(m => m.Position = It.Is<Vec>(v => v.Values[0] == 5 && v.Values[1] == 8));
+    }
+
+    [Fact]
+    public void TestPositionGetThrowsException()
+    {
+        var moving = new Mock<IMoving>();
+        moving.SetupGet(m => m.Position).Throws<Exception>();
+        moving.SetupGet(m => m.Velocity).Returns(new Vec(new int[] { -7, 3 }));
+        var cmd = new MoveCommand(moving.Object);
+        Assert.Throws<Exception>(
+            () => cmd.Execute()
+        );
+    }
+    [Fact]
+    public void TestPositionSetThrowsException()
+    {
+        var moving = new Mock<IMoving>();
+        moving.SetupSet(m => m.Position = It.IsAny<Vec>()).Callback(() => throw new Exception());
+        moving.SetupGet(m => m.Position).Returns(new Vec(new int[] { 12, 5 }));
+        moving.SetupGet(m => m.Velocity).Returns(new Vec(new int[] { -7, 3 }));
+        var cmd = new MoveCommand(moving.Object);
+        Assert.Throws<Exception>(
+            () => cmd.Execute()
+        );
+
+    }
+    [Fact]
+    public void TestVelocityGetThrowsException()
+    {
+        var moving = new Mock<IMoving>();
+        moving.SetupGet(m => m.Position).Returns(new Vec(new int[] { 12, 5 }));
+        moving.SetupGet(m => m.Velocity).Throws<Exception>();
+        var cmd = new MoveCommand(moving.Object);
+        Assert.Throws<Exception>(
+            () => cmd.Execute()
+        );
+    }
+}
