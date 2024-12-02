424a5f0bfb785d528b2af074f23b27dd41bd3eab
Miha <mikhail05@list.ru>
2024-12-02 19:24:11 +0600

    create Rotate, Angle, RotateCommand, RotateTest

---
+++ SpaceBattle.Lib/RotateCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle.Lib
+{
+    public class RotateCommand : ICommand
+    {
+        private readonly IRotating rotating;
+
+        public RotateCommand(IRotating rotating)
+        {
+            this.rotating = rotating;
+        }
+
+        public void Execute()
+        {
+            rotating.Angle = new Angle((rotating.Angle.a + rotating.Velocity) % rotating.Angle.n, rotating.Angle.n);
+        }
+    }
+}
---
+++ SpaceBattle.Tests/RotateTest.cs
@@ -0,0 +1,60 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests
+{
+    public class RotateTest
+    {
+        [Fact]
+        public void TestRotationPositive()
+        {
+            var angle = new Angle(45, 360);
+            var rotating = new Mock<IRotating>();
+            rotating.SetupGet(r => r.Angle).Returns(angle);
+            rotating.SetupGet(r => r.Velocity).Returns(90);
+
+            var cmd = new RotateCommand(rotating.Object);
+            cmd.Execute();
+
+            rotating.VerifySet(r => r.Angle = It.Is<Angle>(v => v.a == 135 && v.n == 360));
+        }
+
+        [Fact]
+        public void TestAngleGetThrowsException()
+        {
+            var rotating = new Mock<IRotating>();
+            rotating.SetupGet(r => r.Angle).Throws<Exception>();
+            rotating.SetupGet(r => r.Velocity).Returns(90);
+
+            var cmd = new RotateCommand(rotating.Object);
+
+            Assert.Throws<Exception>(() => cmd.Execute());
+        }
+
+        [Fact]
+        public void TestVelocityGetThrowsException()
+        {
+            var rotating = new Mock<IRotating>();
+            rotating.SetupGet(r => r.Angle).Returns(new Angle(45, 360));
+            rotating.SetupGet(r => r.Velocity).Throws<Exception>();
+
+            var cmd = new RotateCommand(rotating.Object);
+            Assert.Throws<Exception>(() => cmd.Execute());
+        }
+
+        [Fact]
+        public void TestAngleSetThrowsException()
+        {
+            var angle = new Angle(45, 360);
+            var rotating = new Mock<IRotating>();
+            rotating.SetupGet(r => r.Angle).Returns(angle);
+            rotating.SetupGet(r => r.Velocity).Returns(90);
+
+            rotating.SetupSet(r => r.Angle = It.IsAny<Angle>()).Callback(() => throw new Exception());
+
+            var cmd = new RotateCommand(rotating.Object);
+
+            Assert.Throws<Exception>(() => cmd.Execute());
+        }
+    }
+}
