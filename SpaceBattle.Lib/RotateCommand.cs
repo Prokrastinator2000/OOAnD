270d3d38190ca7f7456ec0d90eec16b0779d1313
Miha <mikhail05@list.ru>
2024-12-15 10:21:38 +0600

    add RotateCommandIoc and RotateIocRegisterTest

---
+++ SpaceBattle.Lib/RotateCommand.cs
@@ -0,0 +1,18 @@
+﻿using App;
+namespace SpaceBattle.Lib
+{
+    public class RotateCommand : ICommand
+    {
+        private readonly IRotating rotating;
+
+        public RotateCommand(IRotating rotating)
+        {
+            this.rotating = rotating;
+        }
+
+        public virtual void Execute()
+        {
+            rotating.Angle = new Angle(rotating.Angle + rotating.Velocity);
+        }
+    }
+}
