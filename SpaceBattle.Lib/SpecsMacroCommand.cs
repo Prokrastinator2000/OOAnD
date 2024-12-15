6a0407b4e4aac4114ce4f01acaf5fe6fc11594d2
Miha <mikhail05@list.ru>
2024-12-15 10:57:00 +0600

    add SpecsMacroCommand and SpecsMacroCommandTests

---
+++ SpaceBattle.Lib/SpecsMacroCommand.cs
@@ -0,0 +1,118 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+
+public class CreateMacroCommandStrategy
+{
+    private readonly string _commandSpec;
+
+    public CreateMacroCommandStrategy(string commandSpec)
+    {
+        _commandSpec = commandSpec;
+    }
+
+    public ICommand Resolve()
+    {
+        try
+        {
+            var commandNames = GetCommandNamesForSpec(_commandSpec);
+
+            if (commandNames == null || !commandNames.Any())
+            {
+                throw new InvalidOperationException($"No commands found for spec: {_commandSpec}");
+            }
+
+            var commands = new List<ICommand>();
+
+            foreach (var commandName in commandNames)
+            {
+                var command = Ioc.Resolve<ICommand>(commandName);
+                if (command == null)
+                {
+                    throw new InvalidOperationException($"Command {commandName} could not be resolved.");
+                }
+
+                commands.Add(command);
+            }
+
+            return new MacroCommand(commands.ToArray());
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException("Error resolving macro command dependencies.", ex);
+        }
+    }
+
+    private IEnumerable<string> GetCommandNamesForSpec(string commandSpec)
+    {
+        if (commandSpec == "Macro.Test")
+        {
+            return new List<string> { "Command1", "Command2", "Commands.Move", "Commands.Rotate" };
+        }
+        else if (commandSpec == "Macro.Extended")
+        {
+            return new List<string> { "Command1", "Command2", "Commands.Move", "Commands.Rotate", "Command3" };
+        }
+        else if (commandSpec == "Specs.Move")
+        {
+            return new List<string> { "Commands.Move" };
+        }
+        else if (commandSpec == "Specs.Rotate")
+        {
+            return new List<string> { "Commands.Rotate" };
+        }
+
+        throw new InvalidOperationException($"Strategy \"{commandSpec}\" not supported.");
+    }
+}
+
+public interface IIocContainer
+{
+    T Resolve<T>(string name);
+}
+
+// public class IocContainer : IIocContainer
+// {
+//     private readonly Dictionary<string, object> _registrations = new();
+
+//     public void Register<T>(string name, T instance)
+//     {
+//         _registrations[name] = instance;
+//     }
+
+//     public T Resolve<T>(string name)
+//     {
+//         return _registrations.ContainsKey(name) ? (T)_registrations[name] : default;
+//     }
+// }
+
+// Пример команд
+public class Command1 : ICommand
+{
+    public virtual void Execute()
+    {
+        Console.WriteLine("Command1 executed.");
+    }
+}
+
+public class Command2 : ICommand
+{
+    public virtual void Execute()
+    {
+        Console.WriteLine("Command2 executed.");
+    }
+}
+
+public class CommandRotate : ICommand
+{
+    public virtual void Execute()
+    {
+        Console.WriteLine("Command.Rotate executed.");
+    }
+}
+public class CommandMove : ICommand
+{
+    public virtual void Execute()
+    {
+        Console.WriteLine("Command.Move executed.");
+    }
+}
---
+++ SpaceBattle.Tests/SpecsMacroCommandTests.cs
@@ -0,0 +1,52 @@
+﻿using App;
+using Moq;
+namespace SpaceBattle.Lib.Tests;
+
+public class MacroCommandTests
+{
+    [Fact]
+    public void Test_Resolve_MacroCommand_Success()
+    {
+        IEnumerable<string> CmdNames = new List<string> { "Command1", "Command2", "Command.Move", "Commands.Rotate" };
+
+        var cmd1 = new Mock<Command1>();
+        var cmd2 = new Mock<Command2>();
+        var cmd3 = new Mock<CommandMove>();
+        var cmd4 = new Mock<CommandRotate>();
+
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Command1",
+                            (object[] args) => cmd1.Object).Execute();
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Command2",
+                            (object[] args) => cmd2.Object).Execute();
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Commands.Move",
+                            (object[] args) => cmd3.Object).Execute();
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Commands.Rotate",
+                            (object[] args) => cmd4.Object).Execute();
+
+        ICommand[] list = [cmd1.Object, cmd2.Object, cmd3.Object, cmd4.Object];
+        var SourceMacroCommand = new MacroCommand(list);
+
+        var strategy = new CreateMacroCommandStrategy("Macro.Test");
+
+        var macroCommand = strategy.Resolve();
+
+        macroCommand.Execute();
+
+        cmd1.Verify(x => x.Execute());
+        cmd2.Verify(x => x.Execute());
+        cmd3.Verify(x => x.Execute());
+        cmd4.Verify(x => x.Execute());
+    }
+
+    [Fact]
+    public void Test_Resolve_MacroCommand_Failure()
+    {
+        var strategy = new CreateMacroCommandStrategy("Macro.Extended");
+
+        Assert.Throws<InvalidOperationException>(() => strategy.Resolve());
+    }
+}
