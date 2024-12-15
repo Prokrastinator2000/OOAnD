93d4c8a4116f1982965d0028d324cb546c51f83d
Miha <mikhail05@list.ru>
2024-12-15 10:05:54 +0600

    add Rotate ICommand Rotate RotateTest

---
+++ SpaceBattle.Lib/RotateCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle.Lib
+{
+    public class RotateCommand : ICommand
+    {
+        private readonly IRotating rotating;
+
+        public RotateCommand(IRotating rotating)
+        {
+            this.rotating = rotating;
+        }
+
+        public virtual void Execute()
+        {
+            rotating.Angle = new Angle(rotating.Angle + rotating.Velocity);
+        }
+    }
+}
