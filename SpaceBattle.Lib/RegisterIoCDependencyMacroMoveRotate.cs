1c478bb3541d3713c1bf98961b66fe7eb18e6039
Miha <mikhail05@list.ru>
2024-12-22 20:30:34 +0600

    add tests

---
+++ SpaceBattle.Lib/RegisterIoCDependencyMacroMoveRotate.cs
@@ -0,0 +1,17 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+public class RegisterIoCDependencyMacroMoveRotate : ICommand
+{
+    public void Execute()
+    {
+        var CreateMacro = new CreateMacroCommandStrategy("Macro.Move");
+        Ioc.Resolve<App.ICommand>("IoC.Register",
+                    "Macro.Move",
+                    (object[] args) => CreateMacro.Resolve(args)).Execute();
+
+        CreateMacro = new CreateMacroCommandStrategy("Macro.Rotate");
+        Ioc.Resolve<App.ICommand>("IoC.Register",
+                    "Macro.Rotate",
+                    (object[] args) => CreateMacro.Resolve(args)).Execute();
+    }
+}
