f7fdfc9eef6602d6e6e395d35e1e53c48a20a967
Miha <mikhail05@list.ru>
2024-12-22 19:06:23 +0600

    add RegisterMacroMoveRotate

---
+++ SpaceBattle.Lib/RegisterIoCDependencyMacroMoveRotate.cs
@@ -0,0 +1,18 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+public class RegisterIoCDependencyMacroMoveRotate : ICommand
+{
+    public void Execute()
+    {
+        RegisterIoCDepedencyMacroByStrategy("Move");
+        RegisterIoCDepedencyMacroByStrategy("Rotate");
+    }
+    private void RegisterIoCDepedencyMacroByStrategy(string StrategyName)
+    {
+        var CreateMacroMove = new CreateMacroCommandStrategy(StrategyName);
+        Ioc.Resolve<App.ICommand>("IoC.Register",
+                    "Macro." + StrategyName,
+                    (object[] args) => CreateMacroMove.Resolve(args)).Execute();
+
+    }
+}
---
+++ SpaceBattle.Tests/RegisterIoCDependencyMacroMoveRotateTests.cs
@@ -0,0 +1,59 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+namespace SpaceBattle.Lib.Tests;
+
+public class RegisterIoCDependencyMacroMoveRotateTests
+{
+    public RegisterIoCDependencyMacroMoveRotateTests()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+    [Fact]
+    public void Execute()
+    {
+        var cmd1 = new Mock<ICommand>();
+        var cmd2 = new Mock<ICommand>();
+        var cmd3 = new Mock<ICommand>();
+
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Command1",
+                            (object[] args) => cmd1.Object).Execute();
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Command.Move",
+                            (object[] args) => cmd2.Object).Execute();
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Command.Rotate",
+                            (object[] args) => cmd3.Object).Execute();
+
+        var RegisterMacroCmd = new RegisterIoCDependencyMacroCommand();
+        RegisterMacroCmd.Execute();
+
+        IEnumerable<string> MacroTestDependencies = new List<string> { "Command.Move" };
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Specs.Macro.Move",
+                            (object[] args) => MacroTestDependencies).Execute();
+        var strategy = new CreateMacroCommandStrategy("Macro.Move");
+        var macroCommand = strategy.Resolve(new object[0]);
+        macroCommand.Execute();
+
+        cmd1.Verify(c => c.Execute(), Times.Never);
+        cmd2.Verify(c => c.Execute(), Times.Once);
+        cmd3.Verify(c => c.Execute(), Times.Never);
+
+        MacroTestDependencies = new List<string> { "Command.Rotate" };
+        Ioc.Resolve<ICommand>("IoC.Register",
+                            "Specs.Macro.Rotate",
+                            (object[] args) => MacroTestDependencies).Execute();
+        strategy = new CreateMacroCommandStrategy("Macro.Rotate");
+        macroCommand = strategy.Resolve(new object[0]);
+        macroCommand.Execute();
+
+        cmd1.Verify(c => c.Execute(), Times.Never);
+        cmd2.Verify(c => c.Execute(), Times.Once);
+        cmd3.Verify(c => c.Execute(), Times.Once);
+
+    }
+}
