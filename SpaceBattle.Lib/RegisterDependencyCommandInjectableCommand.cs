8524a445f01afc00007e7343af6b2a9f45c6e22d
Ilin Maxim <macsimcirer@mail.ru>
2024-12-24 21:26:30 +0600

    feat: add registerDependencyOfInjectableCommand

---
+++ SpaceBattle.Lib/RegisterDependencyCommandInjectableCommand.cs
@@ -0,0 +1,10 @@
+﻿using App;
+
+namespace SpaceBattle.Lib;
+public class RegisterDependencyCommandInjectableCommand : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<ICommand>("IoC.Register", "Commands.CommandInjectable", (object[] args) => new InjectableCommand()).Execute();
+    }
+}
---
+++ SpaceBattle.Tests/RegisterDependencyCommandInjectableCommandTests.cs
@@ -0,0 +1,30 @@
+﻿using App;
+using App.Scopes;
+namespace SpaceBattle.Tests;
+public class RegisterDependencyCommandInjectableCommandTests
+{
+    [Fact]
+    public void TestInjectableCommandRegistration()
+    {
+        // Инициализация IoC и скоупов
+        new InitCommand().Execute();
+        var scope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", scope).Execute();
+
+        // Регистрация CommandInjectableCommand
+        Ioc.Resolve<ICommand>(
+           "IoC.Register",
+            "Commands.CommadInjectable",
+            (object[] args) => new InjectableCommand()
+        ).Execute();
+
+        // Проверка
+        var command = Ioc.Resolve<ICommand>("Commands.CommadInjectable");
+        var injectable = Ioc.Resolve<ICommandInjectable>("Commands.CommadInjectable");
+        var commandInjectableCommand = Ioc.Resolve<InjectableCommand>("Commands.CommadInjectable");
+
+        Assert.NotNull(command);
+        Assert.NotNull(injectable);
+        Assert.NotNull(commandInjectableCommand);
+    }
+}
