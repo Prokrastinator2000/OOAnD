ca59f9e650f6206e845a184a87a622ec7d957d8a
Miha <mikhail05@list.ru>
2025-04-30 09:11:30 +0600

    AddAdapterGenerator

---
+++ SpaceBattle.Lib/AdapterGenerator.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection;
+using Scriban;
+
+namespace SpaceBattle;
+
+public class AdapterBuilder
+{
+    private readonly string _adapterName;
+    private readonly string _adapterTypeName;
+    private readonly string _targetTypeName;
+    private readonly List<PropertyTemplateModel> _properties = new();
+
+    public AdapterBuilder(Type adapterType, Type targetType)
+    {
+        _adapterName = adapterType.Name;
+        _adapterTypeName = adapterType.Name;
+        _targetTypeName = FormatGenericType(targetType);
+    }
+
+    public void CreateProperty(PropertyInfo p)
+    {
+        string? get = null, set = null;
+
+        var propertyTypeName = FormatGenericType(p.PropertyType);
+
+        if (p.CanRead)
+        {
+            get = $"return IoC.Resolve<{propertyTypeName}>(\"Game.{p.Name}.Get\", target);";
+            //get = $"Helow";
+        }
+
+        if (p.CanWrite)
+        {
+            set = $"IoC.Resolve<_ICommand.ICommand>(\"Game.{p.Name}.Set\", target, value).Execute();";
+        }
+
+        _properties.Add(new PropertyTemplateModel
+        {
+            Name = p.Name,
+            Type = propertyTypeName,
+            Getter = get,
+            Setter = set
+        });
+    }
+
+    private static string FormatGenericType(Type type)
+    {
+        var typeName = type.Name;
+
+        if (type.IsGenericType)
+        {
+            typeName = typeName[..typeName.IndexOf('`')];
+            var typeArgs = type.GetGenericArguments().Select(FormatGenericType).ToArray();
+            typeName = $"{typeName}<{string.Join(", ", typeArgs)}>";
+        }
+
+        return typeName;
+    }
+
+    public string Build()
+    {
+        const string templateText = @"class {{ adapter_name }}Adapter : {{ adapter_type_name }} {
+            {{ target_type_name }} target;
+            public {{ adapter_name }}Adapter({{ target_type_name }} target) => this.target = target;{{ for prop in properties }}
+            public {{ prop.type }} {{ prop.name }} {
+                {{ if prop.setter}}set { {{ prop.setter }} }{{ end }}
+                {{ if prop.getter}}get { {{ prop.getter }} }{{ end }}
+            }{{ end }}
+        }";
+
+        var template = Template.Parse(templateText);
+        var result = template.Render(new
+        {
+            adapter_name = _adapterName,
+            adapter_type_name = _adapterTypeName,
+            target_type_name = _targetTypeName,
+            properties = _properties
+        });
+
+        return result;
+    }
+
+    private class PropertyTemplateModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string? Getter { get; set; } = string.Empty;
+        public string? Setter { get; set; } = string.Empty;
+    }
+}
---
+++ SpaceBattle.Tests/AdapterGeneratorTests.cs
@@ -0,0 +1,80 @@
+﻿namespace SpaceBattle.Test;
+
+using App;
+using App.Scopes;
+using SpaceBattle.Lib;
+using Xunit;
+
+public class AdapterGeneratorTests
+{
+    public AdapterGeneratorTests()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+        Ioc.Resolve<ICommand>("IoC.Register", "Game.Reflection.GenerateAdapterCode", (object[] args) =>
+        {
+            var adapterType = (Type)args[0];
+            var targetType = (Type)args[1];
+
+            var builder = new AdapterBuilder(adapterType, targetType);
+
+            foreach (var prop in adapterType.GetProperties())
+            {
+                builder.CreateProperty(prop);
+            }
+
+            return builder.Build();
+        }).Execute();
+    }
+
+    [Fact]
+    public void AdapterCodeGeneratorTest_ForIMoving()
+    {
+        var expectedIMovingAdapterCode =
+        """
+        class IMovingAdapter : IMoving {
+                    Vec target;
+                    public IMovingAdapter(Vec target) => this.target = target;
+                    public Vec Position {
+                        set { IoC.Resolve<_ICommand.ICommand>("Game.Position.Set", target, value).Execute(); }
+                        get { return IoC.Resolve<Vec>("Game.Position.Get", target); }
+                    }
+                    public Vec Velocity {
+                        
+                        get { return IoC.Resolve<Vec>("Game.Velocity.Get", target); }
+                    }
+                }
+        """.Replace("\r\n", "\n").Trim();
+
+        var generatedIMovingCode = Ioc.Resolve<string>(
+            "Game.Reflection.GenerateAdapterCode",
+            typeof(IMoving),
+            typeof(Vec),
+            typeof(Vec)
+        ).Replace("\r\n", "\n").Trim();
+
+        Assert.Equal(expectedIMovingAdapterCode, generatedIMovingCode);
+    }
+
+    [Fact]
+    public void AdapterCodeGeneratorTest_ForMoveCommand()
+    {
+        var expectedMoveCommandAdapterCode =
+        """
+        class MoveCommandAdapter : MoveCommand {
+                    IMoving target;
+                    public MoveCommandAdapter(IMoving target) => this.target = target;
+                }
+        """.Replace("\r\n", "\n").Trim();
+
+        var generatedMoveCommandCode = Ioc.Resolve<string>(
+            "Game.Reflection.GenerateAdapterCode",
+            typeof(MoveCommand),
+            typeof(IMoving)
+        ).Replace("\r\n", "\n").Trim();
+
+        Assert.Equal(expectedMoveCommandAdapterCode, generatedMoveCommandCode);
+    }
+}
